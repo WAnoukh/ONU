20ac592078fd8932ba9d6450a5c5873cafe3cd57
AnoukhW <anoukhman@gmail.com>
2025-07-30 21:31:34 +0200

    Made entity related work into theirs own functions

--- src/main.c
+++ src/main.c
@@ -12,6 +12,26 @@ struct entity *entities;
 int entity_count;
 
 double last_time;
+double new_time;
+float delta_time;
+
+void initialize_entities()
+{
+    entity_count = 2;
+    entities = (struct entity*)calloc(entity_count, sizeof(struct entity));
+    struct entity *player = entities;
+    player->type = Player;
+    player->color[0] = 1;
+    struct entity *guard = entities+1;
+    struct entity ent = *guard;
+    guard->type = Guard;
+    guard->color[1] = 1;
+}
+
+void update_entities()
+{
+    entities[0].pos[0] = sinf((float)new_time);
+}
 
 int main()
 {
@@ -25,34 +45,24 @@ int main()
     initialize_renderer();
     last_time = glfwGetTime();
 
-
-    entity_count = 2;
-    entities = (struct entity*)calloc(entity_count, sizeof(struct entity));
-    struct entity *player = entities;
-    player->type = Player;
-    player->color[0] = 1;
-    struct entity *guard = entities+1;
-    struct entity ent = *guard;
-    guard->type = Guard;
-    guard->pos[0] = 0.5; guard->pos[1] = 0;
-    guard->color[1] = 1;
+    initialize_entities();
 
     int acc = 0;
 
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
-        double new_time = glfwGetTime();
-        float delta_time = (float)(new_time - last_time);
+        new_time = glfwGetTime();
+        delta_time = (float)(new_time - last_time);
         last_time = new_time;
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        update_entities();
         //render_triangle();
         render_entities(entities, entity_count);
 
-        guard->pos[0] = sinf((float)new_time);
         acc++;
         glfwSwapBuffers(window);
         glfwPollEvents();
